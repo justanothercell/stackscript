c9ffc7814b3625b9dccf97275ed3888880bf3fcf
DragonFighter603 <kuhn.christopher.a@gmail.com>
2022-02-17 18:27:47 +0100

    a working, (yet polluted) thing (unscoped/branched only)

--- console.py
+++ console.py
@@ -1,23 +1,34 @@
 import sys
 import shlex
 
-from interpreter import Interpreter
+from interpreter import Interpreter, InvalidToken
 
 
-def run_console(debug=False):
-    interpreter = Interpreter(debug=debug)
+def run_console():
+    interpreter = Interpreter()
     while True:
         try:
-            print('# ' if len(interpreter.layers) < 1 else '. ' * len(interpreter.layers), end='')
+            print('# ' if interpreter.layer == 0 else '. ' * interpreter.layer, end='')
             inp = input()
-            tokens = shlex.split(inp, posix=False)
+            try:
+                tokens = shlex.split(inp, posix=False)
+            except ValueError as e:
+                sys.stderr.write(str(e) + '\n')
+                sys.stderr.flush()
+                print('', end='')
+                continue
             for token in tokens:
                 interpreter.add_token(token)
-        except Exception as e:
-            sys.stderr.write(str(e)+'\n')
+                interpreter.execute_all()
+        except InvalidToken as i:
+            sys.stderr.write(f'Invalid token "{str(i)}"\n')
+            sys.stderr.flush()
+            print('', end='')
+        except AssertionError as e:
+            sys.stderr.write(str(e) + '\n')
             sys.stderr.flush()
             print('', end='')
 
 
 if __name__ == '__main__':
-    run_console(True)
+    run_console()
