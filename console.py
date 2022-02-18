bae08ab96e3aa9a8615d1696111b2176087e5784
DragonFighter603 <kuhn.christopher.a@gmail.com>
2022-02-18 18:53:01 +0100

    added support for functions and running from files

--- console.py
+++ console.py
@@ -1,34 +1,10 @@
-import sys
-import shlex
-
-from interpreter import Interpreter, InvalidToken
+from interpreter import Interpreter
 
 
-def run_console():
+def run_console(stack_max=16, debug=False):
     interpreter = Interpreter()
-    while True:
-        try:
-            print('# ' if interpreter.layer == 0 else '. ' * interpreter.layer, end='')
-            inp = input()
-            try:
-                tokens = shlex.split(inp, posix=False)
-            except ValueError as e:
-                sys.stderr.write(str(e) + '\n')
-                sys.stderr.flush()
-                print('', end='')
-                continue
-            for token in tokens:
-                interpreter.add_token(token)
-                interpreter.execute_all()
-        except InvalidToken as i:
-            sys.stderr.write(f'Invalid token "{str(i)}"\n')
-            sys.stderr.flush()
-            print('', end='')
-        except AssertionError as e:
-            sys.stderr.write(str(e) + '\n')
-            sys.stderr.flush()
-            print('', end='')
+    interpreter.run_repl(stack_max=stack_max, debug=debug)
 
 
 if __name__ == '__main__':
-    run_console()
+    run_console(debug=False)
